651c36c6784f97ef42e539aa631aaba773babe1b
Uneebbhat <uneebbhatti3@gmail.com>
2024-11-21 20:02:46 +0500

    Shop login API created

--- src/routes/shop/shopRouter.routes.ts
+++ src/routes/shop/shopRouter.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createShop } from "../../controllers/shop/shopController.controller";
+import { createShop, shopLogin } from "../../controllers/shop/shopController.controller";
 
 const router = Router();
 
 router.post("/v1/create-shop", createShop);
+router.post("/v1/shop-login", shopLogin);
 
 export default router;
